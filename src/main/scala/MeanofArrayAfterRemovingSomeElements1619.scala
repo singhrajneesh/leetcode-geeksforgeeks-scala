f6ccb84fe448fd74fe59625a8104b5fef79a8015
Rajneesh <rajneesh@metlife.com>
2021-05-30 04:35:29 -0400

    1619

---
+++ src/main/scala/MeanofArrayAfterRemovingSomeElements1619.scala
@@ -0,0 +1,25 @@
+object MeanofArrayAfterRemovingSomeElements1619 {
+  def trimMean(arr: Array[Int]): Double = {
+    val numToBeRemoved=(arr.size*0.05).toInt
+    val newArr=quickSort(arr)
+    for(i<- 0 to numToBeRemoved-1)
+    {
+      newArr(i)=0
+      newArr(newArr.size-1-i)=0
+    }
+    newArr.sum/(newArr.size-(2*numToBeRemoved)).toDouble
+  }
+
+  def quickSort(arr:Array[Int]):Array[Int]={
+    if(arr.size<=1)
+      return arr
+    else {
+      val pivot=arr(arr.size/2)
+      Array.concat(
+        quickSort(arr.filter(_<pivot)),
+        arr.filter(_==pivot),
+        quickSort(arr.filter(_>pivot))
+      )
+    }
+  }
+}
