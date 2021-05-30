c97b5f45195735f1caae37003b398d53fb8e7f40
Rajneesh <rajneesh@metlife.com>
2021-05-30 05:25:31 -0400

    1394

---
+++ src/main/scala/FindLuckyIntegerArray1394.scala
@@ -0,0 +1,12 @@
+object FindLuckyIntegerArray1394 {
+  def findLucky(arr: Array[Int]): Int = {
+    val mapOfFrequency=scala.collection.mutable.Map[Int,Int]()
+    for(i <- 0 to arr.length-1){
+      mapOfFrequency+=(arr(i)->(mapOfFrequency.getOrElse(arr(i),0)+1))
+    }
+    val filtered=mapOfFrequency.filter(x=>x._1==x._2)
+    if(filtered.isEmpty)
+      -1
+    else filtered.toSeq.sortWith((x,y)=>x._1>y._1).head._1
+  }
+}
