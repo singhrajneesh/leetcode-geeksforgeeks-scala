4e3f8f0bf95e06f80acc8cda3ffae47615342b84
Rajneesh <rajneesh@metlife.com>
2021-05-30 01:42:47 -0400

    1399

---
+++ src/main/scala/CountLargestGroup1399.scala
@@ -0,0 +1,16 @@
+object CountLargestGroup1399 {
+    def countLargestGroup(n: Int): Int = {
+      val mapOfSummation=scala.collection.mutable.Map[Int,Int]()
+      for(i <- 1 to n){
+        var num=i
+        var sum=0
+        while(num>0){
+          sum=sum+(num%10)
+          num=num/10
+        }
+        mapOfSummation+=(sum->(mapOfSummation.getOrElse(sum,0)+1))
+      }
+      val count=mapOfSummation.toSeq.sortWith((x,y)=>x._2>y._2).head._2
+      mapOfSummation.count(_._2 == count)
+    }
+}
