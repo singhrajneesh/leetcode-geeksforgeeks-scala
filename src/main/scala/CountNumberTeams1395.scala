c7a9c0853255decde4ae4c33facf823d67843605
Rajneesh <rajneesh@metlife.com>
2021-05-31 13:43:38 -0400

    1395

---
+++ src/main/scala/CountNumberTeams1395.scala
@@ -0,0 +1,23 @@
+object CountNumberTeams1395 {
+
+  def numTeams(rating: Array[Int]): Int = {
+    var count = 0
+    for (i <- rating.indices)
+      for (j <- i + 1 to rating.size - 1) {
+        if (rating(j) > rating(i)) {
+          for (k <- j + 1 to rating.size - 1) {
+            if (rating(k) > rating(j))
+              count += 1
+          }
+        }
+
+        if (rating(j) < rating(i)) {
+          for (k <- j + 1 to rating.size - 1) {
+            if (rating(k) < rating(j))
+              count += 1
+          }
+        }
+      }
+    count
+  }
+}
