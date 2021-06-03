0e1e7ea2597ff2a9e8250b59967157760aac49cb
r289 <rajneesh@metlife.com>
2021-06-03 09:14:31 -0400

    35

---
+++ src/main/scala/array/SearchInsertPosition35.scala
@@ -0,0 +1,11 @@
+package array
+
+object SearchInsertPosition35 {
+  def searchInsert(nums: Array[Int], target: Int): Int = {
+    for (i <- nums.indices)
+      if (nums(i) >= target)
+        return i
+
+    return nums.length
+  }
+}
